6542f683fdfa0bb74590c92fcd5fa35671fd2ef1
Hayden <62683492+randine@users.noreply.github.com>
2020-09-23 12:56:55 +1000

    Update outputs.tf
    changed the output file

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,3 @@
 output "kube_config" {
     value = module.cluster.kube_config
 }
-
-#output "cluster_ca_certificate" {
-#    value = module.cluster.cluster_ca_certificate
-#}
-
-#output "client_certificate" {
-#    value = module.cluster.client_certificate
-#}
-
-#output "client_key" {
-#    value = module.cluster.client_key
-#}
-
-#output "host" {
-#    value = module.cluster.host
-#}
